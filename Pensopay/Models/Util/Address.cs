fcd1676fdedb219170f7a0e245e2fe5ee551fde7
Alexander Colmorn <Alex802c@gmail.com>
2023-12-21 19:39:59 +0100

    Added Payment Model + Util for payment

---
+++ Pensopay/Models/Util/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensopay.Models.Util
+{
+    public class Address
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public string zipcode { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string email { get; set; }
+        public string phone_number { get; set; }
+        public string mobile_number { get; set;}
+    }
+}
---
+++ Pensopay/Models/Util/ShippingObj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensopay.Models.Util
+{
+    public class ShippingObj
+    {
+        public double amount { get; set; }
+        public string method { get; set; }
+        public string company { get; set; }
+        public double vat_rate { get; set; }
+    }
+}
