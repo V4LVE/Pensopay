7c0ea2e4b02fc8159b7aeca3cce0dbbdc53c6e26
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2024-03-13 08:54:20 +0100

    Added Integration Test for basket and order

--- Pensopay.IntegrationTests/PaymentServiceTest.cs
+++ Pensopay.IntegrationTests/PaymentServiceTest.cs
@@ -1,6 +1,7 @@
 
 using Pensopay.IntegrationTests.Properties;
 using Pensopay.IntegrationTests.Util;
+using Pensopay.Models.Util;
 using Pensopay.Parameters;
 using Pensopay.RequestParameters;
 using Pensopay.Services;
@@ -75,6 +76,41 @@ namespace Pensopay.IntegrationTests
 
             //Assert
             Assert.True(result != null);
+        }
+
+        [Fact]
+        public void CreatePaymentWithBasket()
+        {
+            //Arrange
+            PaymentService service = new(PensopayConfig.bearerToken);
+            string randomOrderId = OrderIDGenerator.GenerateRandomId();
+
+            Address address = new() { name = "test", address = "test", city = "Copenhagen", country = "Denmark", email = "test@test.dk", mobile_number = "12345678", phone_number = "12345678", zipcode = "2300" };
+
+            Order order = new()
+            {
+                billing_address = address,
+                shipping_address = address,
+                Basket = new() { new Item() {qty = 2, sku = "123test", name = "The Elder Wand", price = 10000, vat_rate = 25 } }
+            };
+
+
+            var reqParams = new CreatePaymentRequestParams()
+            {
+                currency = "DKK",
+                order_id = randomOrderId,
+                amount = 100,
+                order = order
+            };
+
+            var task = service.CreatePaymentAsync(reqParams);
+
+            //Act
+            var result = task.Result;
+
+            //Assert
+            Assert.True(result != null);
+
         }
     }
 }
