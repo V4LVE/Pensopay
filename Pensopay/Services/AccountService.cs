bcf1c3fc9059731045622bd94a5d1e7957415cab
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2024-03-14 07:49:10 +0100

    Added account service to view account details.

---
+++ Pensopay/Services/AccountService.cs
@@ -0,0 +1,39 @@
+﻿using Pensopay.Models;
+using Pensopay.Models.Util;
+using Pensopay.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Pensopay.Services
+{
+    public class AccountService : PensopayRestClient
+    {
+        public AccountService(string bearerToken) : base(bearerToken) { }
+
+
+        /// <summary>
+        /// Get the account details from the API
+        /// </summary>
+        /// <returns>The requested account</returns>
+        public async Task<Account> GetAccountAsync()
+        {
+            return CallEndpointAsync<Account>("account").Result;
+        }
+
+
+        /// <summary>
+        /// Get the available methods from the API
+        /// </summary>
+        /// <returns>A list of strings for the given available methods</returns>
+        public async Task<Methods> GetMethodsAsync()
+        {
+            return CallEndpointAsync<Methods>("methods").Result;
+        }
+    }
+}
