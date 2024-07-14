394d4d810066b42f6c76360a96c00aae17d48f2a
Alexander Colmorn <Alex802c@gmail.com>
2024-07-14 15:59:14 +0200

    Added api logic to settlementService

---
+++ Pensopay/Models/Settlements.cs
@@ -0,0 +1,16 @@
+﻿using Pensopay.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Pensopay.Models
+{
+    public class Settlements
+    {
+        public List<Settlement> data { get; set; }
+        public PageParameters meta { get; set; }
+    }
+}
--- Pensopay/Services/SettlementService.cs
+++ Pensopay/Services/SettlementService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Pensopay.Models;
+using Pensopay.Parameters;
+using Pensopay.Util;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,34 @@ using System.Threading.Tasks;
 
 namespace Pensopay.Services
 {
-    internal class SettlementService
+    public class SettlementService : PensopayRestClient
     {
+        public SettlementService(string bearerToken) : base(bearerToken) { }
+
+        /// <summary>
+        /// Get all settlements from the API
+        /// </summary>
+        /// <param name="pageParameters"></param>
+        /// <returns></returns>
+        public async Task<Settlements> GetSettlementsAsync(PageParameters? pageParameters = null)
+        {
+            Action<RestRequest> preRequest = (request) =>
+            {
+                AddPagingParameters(pageParameters, request);
+            };
+
+
+            return CallEndpointAsync<Settlements>("settlements", preRequest).Result;
+        }
+
+        /// <summary>
+        /// Get a specific settlement from the API
+        /// </summary>
+        /// <param name="settlementId"></param>
+        /// <returns></returns>
+        public async Task<Settlement> GeSettlementAsync(int settlementId)
+        {
+            return CallEndpointAsync<Settlement>($"settlements/{settlementId}").Result;
+        }
     }
 }
