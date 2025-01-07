46ae5fce2276cc3173a4eed3a70b9f060510a6ad
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2025-01-07 08:58:43 +0100

    Fixed multiframework support. Also added GetSubscription

---
+++ Pensopay.IntegrationTests/SubscriptionServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Pensopay.IntegrationTests.Util;
+using Pensopay.Services;
+
+namespace Pensopay.IntegrationTests
+{
+    public class SubscriptionServiceTest
+    {
+        [Fact]
+        public void GetSingleSubscription()
+        {
+            //Arrange
+            SubscriptionService service = new(PensopayConfig.bearerToken);
+            var sub = service.GetSubscription(1000098);
+
+            //Act
+
+            //Assert
+            Assert.True(sub != null);
+        }
+    }
+}
---
+++ Pensopay/Services/SubscriptionService.cs
@@ -0,0 +1,20 @@
+﻿using Pensopay.Models.Subscriptions;
+using Pensopay.Util;
+
+namespace Pensopay.Services
+{
+    public class SubscriptionService : PensopayRestClient
+    {
+        public SubscriptionService(string bearerToken) : base(bearerToken) { }
+
+        /// <summary>
+        /// Retrieve a subscription from the API
+        /// </summary>
+        /// <param name="subId"></param>
+        /// <returns>The requested payment</returns>
+        public Subscription GetSubscription(int subId)
+        {
+            return CallEndpoint<Subscription>($"subscriptions/{subId}");
+        }
+    }
+}
